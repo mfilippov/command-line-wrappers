e573c84b709b67739b3e6c3789fa2faa6df30362
Mikhail Filippov <mikhail@filippov.me>
2020-11-05 10:29:59 +0300

    Add java.cmd and dotnet.cmd scripts.

---
+++ dotnet.cmd
@@ -0,0 +1,149 @@
+:<<"::CMDLITERAL"
+@ECHO OFF
+GOTO :CMDSCRIPT
+::CMDLITERAL
+
+VERSION=dotnet-cmd-v1
+COMPANY_DIR="Mikhail Filippov"
+TARGET_DIR="${TEMPDIR:-/tmp}/$COMPANY_DIR"
+
+warn () {
+    echo "$*"
+}
+
+die () {
+    echo
+    echo "$*"
+    echo
+    exit 1
+}
+
+# OS specific support (must be 'true' or 'false').
+cygwin=false
+msys=false
+darwin=false
+nonstop=false
+case "`uname`" in
+  CYGWIN* )
+    cygwin=true
+    ;;
+  Darwin* )
+    darwin=true
+    ;;
+  MINGW* )
+    msys=true
+    ;;
+  NONSTOP* )
+    nonstop=true
+    ;;
+esac
+
+if [ "$darwin" = "true" ]; then
+    DOTNET_TEMP_FILE=$TARGET_DIR/dotnet-sdk-5.0.100-osx-x64.tar.gz
+    DOTNET_URL=https://download.visualstudio.microsoft.com/download/pr/0871336f-9a83-4ce4-80ca-625d03003369/2eb78456e0b106e9515dc03898d3867a/dotnet-sdk-5.0.100-osx-x64.tar.gz
+    DOTNET_TARGET_DIR=$TARGET_DIR/dotnet-sdk-5.0.100-osx-x64-$VERSION
+else
+    DOTNET_TEMP_FILE=$TARGET_DIR/dotnet-sdk-5.0.100-linux-x64.tar.gz
+    DOTNET_URL=https://download.visualstudio.microsoft.com/download/pr/820db713-c9a5-466e-b72a-16f2f5ed00e2/628aa2a75f6aa270e77f4a83b3742fb8/dotnet-sdk-5.0.100-linux-x64.tar.gz
+    DOTNET_TARGET_DIR=$TARGET_DIR/dotnet-sdk-5.0.100-linux-x64-$VERSION
+fi
+
+set -e
+
+if [ -e "$DOTNET_TARGET_DIR/.flag" ] && [ -n "$(ls "$DOTNET_TARGET_DIR")" ] && [ "x$(cat "$DOTNET_TARGET_DIR/.flag")" = "x${DOTNET_URL}" ]; then
+    # Everything is up-to-date in $DOTNET_TARGET_DIR, do nothing
+    true
+else
+  warn "Downloading $DOTNET_URL to $DOTNET_TEMP_FILE"
+
+  rm -f "$DOTNET_TEMP_FILE"
+  mkdir -p "$TARGET_DIR"
+  if command -v curl >/dev/null 2>&1; then
+      if [ -t 1 ]; then CURL_PROGRESS="--progress-bar"; else CURL_PROGRESS="--silent --show-error"; fi
+      curl $CURL_PROGRESS --output "${DOTNET_TEMP_FILE}" "$DOTNET_URL"
+  elif command -v wget >/dev/null 2>&1; then
+      if [ -t 1 ]; then WGET_PROGRESS=""; else WGET_PROGRESS="-nv"; fi
+      wget $WGET_PROGRESS -O "${DOTNET_TEMP_FILE}" "$DOTNET_URL"
+  else
+      die "ERROR: Please install wget or curl"
+  fi
+
+  warn "Extracting $DOTNET_TEMP_FILE to $DOTNET_TARGET_DIR"
+  rm -rf "$DOTNET_TARGET_DIR"
+  mkdir -p "$DOTNET_TARGET_DIR"
+
+  tar -x -f "$DOTNET_TEMP_FILE" -C "$DOTNET_TARGET_DIR"
+  rm -f "$DOTNET_TEMP_FILE"
+
+  echo "$DOTNET_URL" >"$DOTNET_TARGET_DIR/.flag"
+fi
+
+if [ '!' -e "$DOTNET_TARGET_DIR/dotnet" ]; then
+  die "Unable to find dotnet under $DOTNET_TARGET_DIR"
+fi
+
+exec "$DOTNET_TARGET_DIR/dotnet" "$@"
+
+:CMDSCRIPT
+
+setlocal
+set VERSION=dotnet-cmd-v1
+set COMPANY_NAME=Mikhail Filippov
+set TARGET_DIR=%LOCALAPPDATA%\Temp\%COMPANY_NAME%\
+set DOTNET_TARGET_DIR=%TARGET_DIR%dotnet-sdk-5.0.100-win-x64-%VERSION%\
+set DOTNET_TEMP_FILE=dotnet-sdk-5.0.100-win-x64.zip
+set DOTNET_URL=https://download.visualstudio.microsoft.com/download/pr/7b78bdaa-d0ac-41c4-9fdc-5820d7dc79b6/cea499dd314ba6394ccea51a2a2dcda9/dotnet-sdk-5.0.100-win-x64.zip
+
+
+set POWERSHELL=%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe
+
+if not exist "%DOTNET_TARGET_DIR%" MD "%DOTNET_TARGET_DIR%"
+
+if not exist "%DOTNET_TARGET_DIR%.flag" goto downloadAndExtractDotNet
+
+set /p CURRENT_FLAG=<"%DOTNET_TARGET_DIR%.flag"
+if "%CURRENT_FLAG%" == "%DOTNET_URL%" goto continueWithDotNet
+
+:downloadAndExtractDotNet
+
+CD "%TARGET_DIR%"
+if errorlevel 1 goto fail
+
+echo Downloading %DOTNET_URL% to %TARGET_DIR%%DOTNET_TEMP_FILE%
+if exist "%DOTNET_TEMP_FILE%" DEL /F "%DOTNET_TEMP_FILE%"
+"%POWERSHELL%" -nologo -noprofile -Command "Set-StrictMode -Version 3.0; $ErrorActionPreference = \"Stop\"; (New-Object Net.WebClient).DownloadFile('%DOTNET_URL%', '%DOTNET_TEMP_FILE%')"
+if errorlevel 1 goto fail
+
+RMDIR /S /Q "%DOTNET_TARGET_DIR%"
+if errorlevel 1 goto fail
+
+MKDIR "%DOTNET_TARGET_DIR%"
+if errorlevel 1 goto fail
+
+CD "%DOTNET_TARGET_DIR%"
+if errorlevel 1 goto fail
+
+echo Extracting %TARGET_DIR%%DOTNET_TEMP_FILE% to %DOTNET_TARGET_DIR%
+"%POWERSHELL%" -nologo -noprofile -command "Set-StrictMode -Version 3.0; $ErrorActionPreference = \"Stop\"; Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('..\\..\\%DOTNET_TEMP_FILE%', '.');"
+if errorlevel 1 goto fail
+
+DEL /F "..\..\%DOTNET_TEMP_FILE%"
+if errorlevel 1 goto fail
+
+echo %DOTNET_URL%>"%DOTNET_TARGET_DIR%.flag"
+if errorlevel 1 goto fail
+
+:continueWithDotNet
+
+if not exist "%DOTNET_TARGET_DIR%\dotnet.exe" (
+  echo Unable to find dotnet.exe under %DOTNET_TARGET_DIR%
+  goto fail
+)
+
+call "%DOTNET_TARGET_DIR%\dotnet.exe" %*
+exit /B %ERRORLEVEL%
+endlocal
+
+:fail
+echo "FAIL"
+exit /b 1
---
+++ java.cmd
@@ -0,0 +1,160 @@
+:<<"::CMDLITERAL"
+@ECHO OFF
+GOTO :CMDSCRIPT
+::CMDLITERAL
+
+set -eux
+
+VERSION=java-cmd-v1
+COMPANY_DIR="Mikhail Filippov"
+TARGET_DIR="${TEMPDIR:-/tmp}/$COMPANY_DIR"
+
+warn () {
+    echo "$*"
+}
+
+die () {
+    echo
+    echo "$*"
+    echo
+    exit 1
+}
+
+# OS specific support (must be 'true' or 'false').
+cygwin=false
+msys=false
+darwin=false
+nonstop=false
+case "`uname`" in
+  CYGWIN* )
+    cygwin=true
+    ;;
+  Darwin* )
+    darwin=true
+    ;;
+  MINGW* )
+    msys=true
+    ;;
+  NONSTOP* )
+    nonstop=true
+    ;;
+esac
+
+if [ "$darwin" = "true" ]; then
+    JVM_TEMP_FILE=$TARGET_DIR/jvm-macosx-x64.tar.gz
+    JVM_URL=https://corretto.aws/downloads/resources/11.0.9.12.1/amazon-corretto-11.0.9.12.1-macosx-x64.tar.gz
+    JVM_TARGET_DIR=$TARGET_DIR/amazon-corretto-11.0.4.11.1-macosx-x64-$VERSION
+else
+    JVM_TEMP_FILE=$TARGET_DIR/jvm-linux-x64.tar.gz
+    JVM_URL=https://corretto.aws/downloads/resources/11.0.9.12.1/amazon-corretto-11.0.9.12.1-linux-x64.tar.gz
+    JVM_TARGET_DIR=$TARGET_DIR/amazon-corretto-11.0.4.11.1-linux-x64-$VERSION
+fi
+
+set -e
+
+if [ -e "$JVM_TARGET_DIR/.flag" ] && [ -n "$(ls "$JVM_TARGET_DIR")" ] && [ "x$(cat "$JVM_TARGET_DIR/.flag")" = "x${JVM_URL}" ]; then
+    # Everything is up-to-date in $JVM_TARGET_DIR, do nothing
+    true
+else
+  warn "Downloading $JVM_URL to $JVM_TEMP_FILE"
+
+  rm -f "$JVM_TEMP_FILE"
+  mkdir -p "$TARGET_DIR"
+  if command -v curl >/dev/null 2>&1; then
+      if [ -t 1 ]; then CURL_PROGRESS="--progress-bar"; else CURL_PROGRESS="--silent --show-error"; fi
+      curl $CURL_PROGRESS --output "${JVM_TEMP_FILE}" "$JVM_URL"
+  elif command -v wget >/dev/null 2>&1; then
+      if [ -t 1 ]; then WGET_PROGRESS=""; else WGET_PROGRESS="-nv"; fi
+      wget $WGET_PROGRESS -O "${JVM_TEMP_FILE}" "$JVM_URL"
+  else
+      die "ERROR: Please install wget or curl"
+  fi
+
+  warn "Extracting $JVM_TEMP_FILE to $JVM_TARGET_DIR"
+  rm -rf "$JVM_TARGET_DIR"
+  mkdir -p "$JVM_TARGET_DIR"
+
+  tar -x -f "$JVM_TEMP_FILE" -C "$JVM_TARGET_DIR"
+  rm -f "$JVM_TEMP_FILE"
+
+  echo "$JVM_URL" >"$JVM_TARGET_DIR/.flag"
+fi
+
+JAVA_HOME=
+for d in "$JVM_TARGET_DIR" "$JVM_TARGET_DIR"/* "$JVM_TARGET_DIR/Contents/Home" "$JVM_TARGET_DIR/"*/Contents/Home; do
+  echo "$d"
+  if [ -e "$d/bin/java" ]; then
+    JAVA_HOME="$d"
+  fi
+done
+
+if [ '!' -e "$JAVA_HOME/bin/java" ]; then
+  die "Unable to find bin/java under $JVM_TARGET_DIR"
+fi
+
+JAVA_HOME=$JAVA_HOME exec "$JAVA_HOME/bin/java" "$@"
+
+:CMDSCRIPT
+
+if exist "%JAVA_HOME%\bin\java.exe" goto continueWithJavaHome
+
+setlocal
+set VERSION=java-cmd-v1
+set COMPANY_NAME=Mikhail Filippov
+set TARGET_DIR=%LOCALAPPDATA%\Temp\%COMPANY_NAME%\
+set JVM_TARGET_DIR=%TARGET_DIR%amazon-corretto-11.0.9.12.1-windows-x64-jdk-%VERSION%\
+set JVM_TEMP_FILE=jvm-windows-x64.zip
+set JVM_URL=https://corretto.aws/downloads/resources/11.0.9.12.1/amazon-corretto-11.0.9.12.1-windows-x64-jdk.zip
+
+set POWERSHELL=%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe
+
+if not exist "%JVM_TARGET_DIR%" MD "%JVM_TARGET_DIR%"
+
+if not exist "%JVM_TARGET_DIR%.flag" goto downloadAndExtractJvm
+
+set /p CURRENT_FLAG=<"%JVM_TARGET_DIR%.flag"
+if "%CURRENT_FLAG%" == "%JVM_URL%" goto continueWithJvm
+
+:downloadAndExtractJvm
+
+CD "%TARGET_DIR%"
+if errorlevel 1 goto fail
+
+echo Downloading %JVM_URL% to %TARGET_DIR%%JVM_TEMP_FILE%
+if exist "%JVM_TEMP_FILE%" DEL /F "%JVM_TEMP_FILE%"
+"%POWERSHELL%" -nologo -noprofile -Command "Set-StrictMode -Version 3.0; $ErrorActionPreference = \"Stop\"; (New-Object Net.WebClient).DownloadFile('%JVM_URL%', '%JVM_TEMP_FILE%')"
+if errorlevel 1 goto fail
+
+RMDIR /S /Q "%JVM_TARGET_DIR%"
+if errorlevel 1 goto fail
+
+MKDIR "%JVM_TARGET_DIR%"
+if errorlevel 1 goto fail
+
+CD "%JVM_TARGET_DIR%"
+if errorlevel 1 goto fail
+
+echo Extracting %TARGET_DIR%%JVM_TEMP_FILE% to %JVM_TARGET_DIR%
+"%POWERSHELL%" -nologo -noprofile -command "Set-StrictMode -Version 3.0; $ErrorActionPreference = \"Stop\"; Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('..\\..\\%JVM_TEMP_FILE%', '.');"
+if errorlevel 1 goto fail
+
+DEL /F "..\..\%JVM_TEMP_FILE%"
+if errorlevel 1 goto fail
+
+echo %JVM_URL%>"%JVM_TARGET_DIR%.flag"
+if errorlevel 1 goto fail
+
+:continueWithJvm
+
+set JAVA_HOME=
+for /d %%d in ("%JVM_TARGET_DIR%"*) do if exist "%%d\bin\java.exe" set JAVA_HOME=%%d
+if not exist "%JAVA_HOME%\bin\java.exe" (
+  echo Unable to find java.exe under %JVM_TARGET_DIR%
+  goto fail
+)
+
+:continueWithJavaHome
+
+call "%JAVA_HOME%\bin\java.exe" %*
+exit /B %ERRORLEVEL%
+endlocal
